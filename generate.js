afb3f7ebcfa8ca5f5e772c1871ec1f4e4744679e
joelkur <jkuronen95@gmail.com>
2022-07-01 17:47:36 +0300

    Add script for generating snippets

---
+++ generate.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const fs = require("fs");
+const templates = require("./templates/details.json");
+
+const TEMPLATES_DIRECTORY = path.join(__dirname, "templates");
+const OUTPUT_PATH = path.join(__dirname, "snippets", "snippets.code-snippets");
+
+function generateSnippet(file) {
+  const fileBasename = path.basename(file, ".template");
+  const { name, ...details } = templates[fileBasename];
+
+  const fileContents = fs.readFileSync(
+    path.join(TEMPLATES_DIRECTORY, file),
+    "utf8",
+  );
+
+  const body = fileContents.split("\n");
+  return [name, { ...details, body }];
+}
+
+function main() {
+  const output = {};
+  for (
+    const file of fs.readdirSync(TEMPLATES_DIRECTORY).filter((file) =>
+      file.endsWith(".template")
+    )
+  ) {
+    const [name, details] = generateSnippet(file);
+    output[name] = details;
+  }
+  fs.writeFileSync(OUTPUT_PATH, JSON.stringify(output, null, 2));
+}
+
+if (require.main === module) {
+  main();
+}
